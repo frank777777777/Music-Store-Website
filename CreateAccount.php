a48bbac205a99e0ce7ae342bb010926df90e94df
frank777777777 <yili.zou93@gmail.com>
2015-10-29 17:49:40 -0400

    Create CreateAccount.php

---
+++ CreateAccount.php
@@ -0,0 +1,27 @@
+<html>
+<h1>Create Account:</h1>
+
+	<style type = "text/css">
+	.container{
+		width: 250px;
+		clear: both;
+	}
+	.container input{
+		width: 100%;
+		clear: both;
+	}
+	</style>
+
+
+<div class = "container">
+<form action = "AddAccount.php" method = "get">
+Username: <input type = "text" name = "username"><br>
+Password: <input type = "password" name = "password"><br>
+Re-Enter Password: <input type = "password" name = "repassword">
+</form>
+</div>
+<input type = "submit">
+	
+
+
+</html>
