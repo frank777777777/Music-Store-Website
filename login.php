fcc45d8fc800ce8b77514b612cc5ca92c7b74f43
frank777777777 <yili.zou93@gmail.com>
2015-10-29 17:49:59 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<link rel="stylesheet" type="text/css" href="style.css">
+<div id="topbar"><b>Login:</b></div>
+<a href="CreateAccount.php">Create Account</a> 
+
+
+<body>
+
+
+<form action = "confirmLogin.php" method = "get">
+Username: <input type = "text" name = "username"><br>
+Password: <input type = "password" name = "password"><br>
+<input type = "submit">
+</form>
+
+
+</body>
+</html>
